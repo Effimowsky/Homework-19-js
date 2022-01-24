e72ecc52251f8d5f7b206e1a32b4612e7895b3dc
Effimowsky <o.effimowsky@gmail.com>
2022-01-24 15:00:35 +0400

    add signup form

--- app.js
+++ app.js
@@ -1,50 +1,54 @@
 const signupForm = document.querySelector("#user-signup");
-const username = document.querySelector("username");
-const email = document.querySelector("email");
-const personalNumber = document.querySelector("personal-number");
-const mobileNumber = document.querySelector("mobile-number");
+const Username = document.querySelector("#username");
+const Email = document.querySelector("#email");
+const personalNumber = document.querySelector("#personal-number");
+const mobileNumber = document.querySelector("#mobile-number");
+const Position = document.querySelector("#position");
 
 const usernameError = document.querySelector("#username-error");
 const emailError = document.querySelector("#email-error");
 const personalNumberError = document.querySelector("#personal-number-error");
 const mobileNumberError = document.querySelector("#mobile-number-error");
+const PositionError = document.querySelector("#position-error");
 
-signupForm.addEventListener("submit", (e) => {
+signupForm.addEventListener("submit", e => {
   e.preventDefault();
   validateUsername();
   validateEmail();
   validatePersonalNumber();
   validateMobileNumber();
+  validatePosition();
 });
 
 function validateUsername() {
-  if (!username.validity.valid) {
-    username.classList.add("has-error");
-    if (username.validity.tooShort) {
+  if (!Username.validity.valid) {
+    Username.classList.add("has-error");
+    if (Username.validity.tooShort) {
       usernameError.innerText = "username must be 5 or more";
     } else {
       usernameError.innerText = "Username is required";
     }
     return false;
   } else {
-    username.classList.remove("has-error");
-    username.classList.add("has-success");
-    username.innerText("");
+    Username.classList.remove("has-error");
+    Username.classList.add("has-success");
+    Username.innerText = "";
     return true;
   }
 }
 
 function validateEmail() {
-  if (!email.validity.valid) {
-    email.classList.add("has-error");
-    if (email.validity.typeMismatch) {
+  if (!Email.validity.valid) {
+    Email.classList.add("has-error");
+    if (Email.validity.typeMismatch) {
       emailError.innerText = "Please enter valid email";
     } else {
-      emailError.innerText = "email is required";
+      emailError.innerText = "Email is required";
     }
     return false;
   } else {
-    email.classList.replace("has-error", "has-success");
+    Email.classList.remove("has-error");
+    Email.classList.add("has-success");
     emailError.innerText = "";
   }
   return true;
@@ -53,25 +57,55 @@ function validateEmail() {
 function validatePersonalNumber() {
   if (!personalNumber.validity.valid) {
     personalNumber.classList.add("has-error");
-    let pattern1 = /^[0-9]{11}$/;
-    if (!pattern1.test(personalNumber.value)) {
+    if (personalNumber.validity.tooShort) {
       personalNumberError.innerText = "Personal number must have 11 length";
+    } else {
+      personalNumberError.innerText = "Personal number is required";
     }
+    return false;
   } else {
-    personalNumber.classList.replace("has-error", "has-success");
+    personalNumber.classList.remove("has-error");
+    personalNumber.classList.add("has-success");
     personalNumberError.innerText = "";
   }
+  return true;
 }
 
 function validateMobileNumber() {
   if (!mobileNumber.validity.valid) {
     mobileNumber.classList.add("has-error");
-    let pattern2 = /^[0-9]{9}$/;
-    if (!pattern2.test(mobileNumber.value)) {
+    if (mobileNumber.validity.tooShort) {
       mobileNumberError.innerText = "Mobile Number must be 9 length";
+    } else {
+      mobileNumberError.innerText = "Mobile number is required";
     }
+    return false;
   } else {
-    mobileNumberError.classList.replace("has-error", "has-success");
+    mobileNumber.classList.remove("has-error");
+    mobileNumber.classList.add("has-success");
     mobileNumberError.innerText = "";
   }
+  return true;
 }
+
+function validatePosition() {
+  if (Position.validity.valid) {
+    Position.classList.add("has-success");
+  }
+}
+
+Username.addEventListener("input", () => {
+  validateUsername();
+});
+Email.addEventListener("input", () => {
+  validateEmail();
+});
+personalNumber.addEventListener("input", () => {
+  validatePersonalNumber();
+});
+mobileNumber.addEventListener("input", () => {
+  validateMobileNumber();
+});
+Position.addEventListener("input", () => {
+  validatePosition();
+});
